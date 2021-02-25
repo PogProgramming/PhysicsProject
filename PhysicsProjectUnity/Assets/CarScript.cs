d732c76394180d6c91ef5c3fddc85fdd2026478f
sycellette <sycellette@gmail.com>
2021-02-25 12:30:54 +1100

    Various physics demonstrations implemented. - Lift creation - Basic car base creation - Chair free-fall collision onto a terrain base

---
+++ PhysicsProjectUnity/Assets/CarScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour
+{
+    //Wheels
+    public HingeJoint backRight;
+    public HingeJoint backLeft;
+    public HingeJoint frontLeft;
+    public HingeJoint frontRight;
+
+    JointMotor backRightMotor;
+    JointMotor backLeftMotor;
+    JointMotor frontLeftMotor;
+    JointMotor frontRightMotor;
+
+
+    void Start()
+    {
+        
+        HingeJoint[] joints = GetComponents<HingeJoint>();
+        {
+            backRight = joints[0];
+            backLeft = joints[1];
+            frontLeft = joints[2];
+            frontRight = joints[3];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
