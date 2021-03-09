4ea2338b798cc2972448d8e7e57fee2f4b60f996
Sycellette <sycellette@gmail.com>
2021-03-10 05:19:13 +1100

    Implemented scene menu

---
+++ PhysicsProjectUnity/Assets/MenuInteraction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInteraction : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit(0);
+    }
+}
